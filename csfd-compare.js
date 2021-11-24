74304295eef99da241325212b039397ff0af41df
Verner Jan <jverner@evektor.cz>
2021-11-24 17:02:11 +0100

    Version bump

--- csfd-compare.js
+++ csfd-compare.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSFD porovnání hodnocení
 // @namespace    csfd.cz
-// @version      0.5.11
+// @version      0.5.11.1
 // @description  Show your own ratings on other users ratings list
 // @author       SonGokussj4
 // @license      GNU GPLv3
@@ -19,7 +19,7 @@
 // @supportURL   https://XXgithub.com/SonGokussj4/GitHub-userscripts/issues
 
 
-const VERSION = 'v0.5.11';
+const VERSION = 'v0.5.11.1';
 const SCRIPTNAME = 'CSFD-Compare';
 const SETTINGSNAME = 'CSFD-Compare-settings';
 const GREASYFORK_URL = 'https://greasyfork.org/cs/scripts/425054-%C4%8Dsfd-compare';
