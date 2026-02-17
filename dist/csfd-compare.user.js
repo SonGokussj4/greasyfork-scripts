a80100adde4129adb4d039a2de40d03f3d9c2c43
Jan Verner <SonGokussj4@centrum.cz>
2026-02-17 19:22:34 +0100

    feat: update metadata to use @include instead of @match in user script

--- dist/csfd-compare.user.js
+++ dist/csfd-compare.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         ČSFD Compare V2
 // @version      0.8.0
-// @date         2026-02-17
 // @namespace    csfd.cz
 // @description  Show your own ratings on other users ratings list
 // @author       Jan Verner <SonGokussj4@centrum.cz>
 // @license      GNU GPLv3
 // @icon         http://img.csfd.cz/assets/b1733/images/apple_touch_icon.png
-// @match        *://*csfd.cz/*
-// @match        *://*csfd.sk/*
+// @include      *csfd.cz/*
+// @include      *csfd.sk/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @require      https://greasyfork.org/scripts/449554-csfd-compare-utils/code/csfd-compare-utils.js?version=1100309
 // @grant        GM_addStyle
